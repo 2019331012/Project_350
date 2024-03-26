48545203c17b953e951e5e47e286f76d2e129509
2019331012 <70182200+2019331012@users.noreply.github.com>
2024-03-26 23:11:43 +0600

    simple modifications
    just simple ones

--- lib/Screens/change_name.dart
+++ lib/Screens/change_name.dart
@@ -11,7 +11,6 @@ class ChangeName extends StatefulWidget {
 }
 
 class _ChangeNameState extends State<ChangeName> {
-  final TextEditingController oldNameController = TextEditingController();
   final TextEditingController newNameController = TextEditingController();
 
   late Map<String, String>? loggedInCred;
@@ -22,54 +21,53 @@ class _ChangeNameState extends State<ChangeName> {
     HiveAdapter.initialize(); // Initialize HiveAdapter
     final userCredProvider = Provider.of<UserCredProvider>(context, listen: false);
     loggedInCred = userCredProvider.cred;
-    oldNameController.text = loggedInCred?['name'] ?? '';
   }
 
-
-  // Function to handle changing the password
+  // Function to handle changing the name
   void changeName() {
-    String oldName = oldNameController.text;
+    String oldName = loggedInCred?['name'] ?? '';
     String newName = newNameController.text;
-    
+
     // Add your validation logic here
-    if (oldName.isEmpty || newName.isEmpty) {
-      // Show error message if any field is empty
+    if (newName.isEmpty) {
+      // Show error message if the new name field is empty
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Please fill in all fields'),
+          content: Text('Please enter a new name'),
           duration: Duration(seconds: 2),
         ),
       );
     } else if (newName == oldName) {
-      // Show error message if new passwords do not match
+      // Show error message if the new name is the same as the old name
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('The Names are the same.'),
+          content: Text('The new name cannot be the same as the old name.'),
           duration: Duration(seconds: 2),
         ),
       );
     } else {
-      // Perform password change logic here (e.g., call API or update database)
-      // After successful password change, show success message
+      // Perform name change logic here (e.g., call API or update database)
+      // After successful name change, show success message
 
-      logger.d('LogginCredentials is $loggedInCred');
+      logger.d('LoggingCredentials: $loggedInCred');
       HiveAdapter.changeName(context, loggedInCred?['email'] ?? '', oldName, newName);
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Name has changed successfully'),
+          content: Text('Name has been changed successfully'),
           duration: Duration(seconds: 2),
         ),
       );
-      
-      // Clear text fields after successful password change
-      //oldNameController.clear();
+
+      // Clear the new name text field after successful name change
       newNameController.clear();
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    String oldName = loggedInCred?['name'] ?? '';
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Change Name'),
@@ -79,16 +77,18 @@ class _ChangeNameState extends State<ChangeName> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: oldNameController,
-              decoration: InputDecoration(labelText: 'Old Name'),
-              obscureText: true,
+            ListTile(
+              title: Text(
+                'Current Name:',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(oldName),
+              enabled: false, // This makes the ListTile uneditable
             ),
             SizedBox(height: 16),
             TextField(
               controller: newNameController,
-              decoration: InputDecoration(labelText: 'New Password'),
-              obscureText: true,
+              decoration: InputDecoration(labelText: 'New Name'),
             ),
             SizedBox(height: 16),
             ElevatedButton(
@@ -103,7 +103,6 @@ class _ChangeNameState extends State<ChangeName> {
 
   @override
   void dispose() {
-    oldNameController.dispose();
     newNameController.dispose();
     super.dispose();
   }
